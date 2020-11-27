f6a30df5be80e28c6717f9506719608896645138
Dony Cisneros <dony.cisne@gmail.com>
2020-11-27 09:48:22 -0300

    Dart 02 - Numbers and Strings

---
+++ 02-numbers_and_strings/numbers.dart
@@ -0,0 +1,25 @@
+void main() {
+  /*
+    Numbers
+  */
+  int employees = 24;
+  double pi = 3.1415;
+  var number = 2.0;
+
+  // Show numbers values
+  print('$employees - $pi - $number');
+
+  /*
+    Strings
+  */
+  String name = 'Dony';
+
+  print(name);
+  // First letter of the name
+  // print('First letter of the name: ${name[0]}');
+  print(name[0]);
+  // Total letters of the name
+  print(name.length);
+  // Last letter of the name
+  print(name[name.length - 1]);
+}
